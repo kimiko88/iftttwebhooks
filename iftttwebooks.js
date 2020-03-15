a3a6904dad5d0f40b2bc5adde56a3b9cb2e4e1dd
kimiko88 <chimien@libero.it>
2020-03-15 16:44:57 +0100

    fix and add tests

--- iftttwebooks.js
+++ iftttwebooks.js
@@ -9,43 +9,44 @@ const iftttwebhooks = {
         host: baseUrl.hostname,
         port: baseUrl.port,
         method,
-        headers: {"content-type": "application/json"}
-      }, async (response) => {
-        if (response.statusCode < 200 || response.statusCode > 299) {
-          console.log(`Failed to load page, status code: ${response.statusCode}`)
-        }
-        const bodyChunks = []
-        response.on('data', chunk => bodyChunks.push(chunk))
-        response.on('end', () => {
-          const body = Buffer.concat(bodyChunks)
-          if (response.headers) {
-            const bodyResponse = body.toString('utf8')
-            // if (hasHeaderAndValue('content-type', 'application/json')) {
-              if(response.statusCode >= 200 || response.statusCode <= 299){
-              try {
-                resolve({ data: bodyResponse ? JSON.parse(bodyResponse) : undefined, headers: response.headers, code: response.statusCode })
-              } catch (err) {
-                console.error(err)
-                resolve(undefined)
-              }
-            }else{
-              resolve({ data: bodyResponse, headers: response.headers, code: response.statusCode })
-            }
-            // } else {
-            //   resolve({ data: bodyResponse, headers: response.headers, code: response.statusCode })
-            // }
-          }
-        })
-        request.on('error', (err) => {
-          console.error(err)
-          reject(err)
-        })
+        headers: { "content-type": "application/json" }
+      }, async (response) => { resolve(this.getResponse(response)) })
+      request.on('error', (err) => {
+        console.error(err)
+        reject(err)
       })
       if (body) {
         request.write(JSON.stringify(body))
       }
       request.end()
     })
+  },
+
+  getResponse(response) {
+    return new Promise((resolve) => {
+      const bodyChunks = []
+      response.on('data', chunk => bodyChunks.push(chunk))
+      response.on('end', () => {
+        const body = Buffer.concat(bodyChunks)
+        if (response.headers) {
+          const bodyResponse = body.toString('utf8')
+          if (this.hasHeaderAndValue(response, 'content-type', 'application/json')) {
+            resolve({ data: bodyResponse ? JSON.parse(bodyResponse) : undefined, headers: response.headers, code: response.statusCode })
+          } else {
+            resolve({ data: bodyResponse, headers: response.headers, code: response.statusCode })
+          }
+        }
+      })
+    })
+  },
+
+  hasHeaderAndValue(response, header, value) {
+    for (var val in response.headers) {
+      if (val.toLowerCase() === header.toLowerCase()) {
+        return response.headers[val].startsWith(value)
+      }
+    }
+    return false
   }
 }
 
--- test/test.js
+++ test/test.js
@@ -8,4 +8,28 @@ describe('iftttwebooks', () => {
         assert.notEqual(res.headers, undefined)
         assert.equal(res.code, 401)
     })
+
+    it('no event and secret', async () => {
+        var res = await iftttwebooks.sendRequest('','', {"d":"d"})
+        assert.equal(res.code, 404)
+    })
+
+    it('no secret', async () => {
+        var res = await iftttwebooks.sendRequest('test','', {"d":"d"})
+        assert.equal(res.code, 404)
+    })
+
+    it('send get', async () => {
+        var res = await iftttwebooks.sendRequest('test','testsecret', undefined, 'GET')
+        assert.notEqual(res.data, undefined, "data is undefined")
+        assert.notEqual(res.headers, undefined)
+        assert.equal(res.code, 401)
+    })
+
+    it('send error', async () => {
+        var res = await iftttwebooks.sendRequest()
+        assert.notEqual(res.data, undefined, "data is undefined")
+        assert.notEqual(res.headers, undefined)
+        assert.equal(res.code, 401)
+    })
 })
